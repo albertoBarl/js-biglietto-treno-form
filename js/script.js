44b3999620e9a8b2903b090c0658d1fe1c1096ef
albertoBarl <alberto.barletta00@gmail.com>
2022-11-28 17:28:02 +0100

    reset

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// 1- Il numero di chilometri da percorrere
+    
+// 2- Età del passeggero
+
+// 3- calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole: 
+
+// 3.1- il prezzo del biglietto è definito in base ai km (0.21 € al km) 
+
+// 3.2- va applicato uno sconto del 20% per i minorenni 
+
+// 3.3- va applicato uno sconto del 40% per gli over 65.
